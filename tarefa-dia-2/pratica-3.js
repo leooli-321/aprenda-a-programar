52f42cdedb391bc0af05df03e75feff4df7154b1
leooli-321 <leooli_artist@outlook.com>
2022-08-25 13:18:12 -0300

    pratica3

---
+++ tarefa-dia-2/pratica-3.js
@@ -0,0 +1,11 @@
+var value="Eu sou uma linha de código muito fofa";
+var letras=value.length;
+
+    console.log(letras)
+
+
+if (letras == 6114) {
+        console.log("Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas. :'(");
+} else {
+        console.log("Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D");
+}
