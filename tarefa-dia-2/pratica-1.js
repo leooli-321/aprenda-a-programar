eccba553490c30abc83de67b0ee3a4098ac5280e
leooli-321 <leooli_artist@outlook.com>
2022-08-24 23:49:24 -0300

    pratica1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,14 @@
 let nome = "Leo Oli"
+    console.log(nome)
+
 let email = "leooli_artist@outlook.com"
+    console.log(email)
+
 let profissao = "CEO"
-let idade = "26"
+    console.log(profissao)
+
+let idade = 26
+    console.log(idade)
+
 let sonho = "Japao"
+    console.log (sonho)
