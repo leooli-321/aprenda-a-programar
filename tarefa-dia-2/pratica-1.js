e9290fe8a73ab2d83e251c83859ad2e9ce822a08
leooli-321 <leooli_artist@outlook.com>
2022-08-25 00:02:35 -0300

    praticaAtualizada

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -12,3 +12,7 @@ let idade = 26
 
 let sonho = "Japao"
     console.log (sonho)
+
+let mensagem = "Olá, " + nome + " ! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das aéreas para o " + sonho +". Confirma pra gente alguns dados? A sua idade é " + idade + ", você é " + profissao + " e o seu email é " + email + "? Ficamos no aguardo. Muito obrigado e boa viagem! :D"
+
+    console.log (mensagem)
