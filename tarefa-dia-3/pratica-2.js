c24022ec018b4ce1ee962c0b90a0ef22515d9464
leooli-321 <leooli_artist@outlook.com>
2022-08-26 19:06:44 -0300

    pratica2finalizada

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,13 @@
+let a = 9
+let b = 1
+
+    console.log ("Aqui está a tabuada do 9: ")
+
+while (a * b < 91) {
+    console.log (a, "x", b, "=", (a * b))
+    b+++ 1;
+
+   // if 
+     //   (b === 91) break;
+    
+  }
